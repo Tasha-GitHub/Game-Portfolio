94946607de29764d09391d709060e8c62750af10
Tasha Casagni <casagni708@hotmail.com>
2016-12-19 01:05:47 -0600

    html build

---
+++ app/public/javascript/index.js
@@ -0,0 +1,11 @@
+
+var game = new Phaser.Game(1000, 600, Phaser.AUTO, "gameCore", { preload: preload, create: create, update: update });
+
+function preload() {
+}
+
+function create() {
+}
+
+function update() {
+}
