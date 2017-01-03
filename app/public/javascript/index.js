0e6dc41c87e84f3c39bb1276d6a77dd0d854d3f6
Tasha Casagni <casagni708@hotmail.com>
2017-01-03 11:51:51 -0600

    building out game core

--- app/public/javascript/index.js
+++ app/public/javascript/index.js
@@ -4,9 +4,20 @@ var player;
 
 function preload() {
 	//preloads all game elements
-	game.load.image('sky', 'images/sky.png');
-    game.load.image('ground', 'images/platform.png');
-    game.load.spritesheet('dude', 'images/baddie.png', 32, 30);
+	game.load.image('sky', 'images/game-core/sky.png');
+    game.load.image('ground', 'images/game-core/platform.png');
+    game.load.image('js', 'images/skills-tree/javascript.png');
+    game.load.image('html', 'images/skills-tree/htmlLogo.png');
+    game.load.image('css', 'images/skills-tree/css.png');
+    game.load.image('github', 'images/skills-tree/github.png');
+    game.load.image('node', 'images/skills-tree/node.png');
+    game.load.image('jquery', 'images/skills-tree/jquery.png');
+    game.load.image('bootstrap', 'images/skills-tree/bootstrap.png');
+    game.load.image('mocha', 'images/skills-tree/mocha.png');
+    game.load.image('chai', 'images/skills-tree/chai.png');
+    game.load.image('react', 'images/skills-tree/react.png');
+    game.load.image('sass', 'images/skills-tree/sass.png');
+    game.load.spritesheet('dude', 'images/game-core/baddie.png', 32, 30);
 }
 
 function create() {
@@ -16,7 +27,10 @@ function create() {
     //  A simple background for our game
     game.add.sprite(0, 0, 'sky');
 
-    //  The platforms group contains the ground and the 2 ledges we can jump on
+    //world bounds
+    game.world.setBounds(0, 0, 4000, 600);
+
+    //  The platforms group contains the ground and ledges
     platforms = game.add.group();
 
     //  We will enable physics for any object that is created in this group
@@ -26,10 +40,33 @@ function create() {
     var ground = platforms.create(0, game.world.height - 64, 'ground');
 
     //  Scale it to fit the width of the game
-    ground.scale.setTo(3, 2);
+    ground.scale.setTo(11, 2);
 
     //  This stops it from falling away when you jump on it
     ground.body.immovable = true;
+
+    //skill-set loaders
+    game.add.sprite(0, 400, 'js');
+
+    game.add.sprite(200, 400, 'html');
+
+    game.add.sprite(400, 400, 'css');
+
+    game.add.sprite(600, 400, 'github');
+
+    game.add.sprite(800, 400, 'jquery');
+
+    game.add.sprite(1000, 400, 'node');
+
+    game.add.sprite(1200, 400, 'bootstrap');
+
+    game.add.sprite(1400, 400, 'mocha');
+
+    game.add.sprite(1600, 400, 'chai');
+
+    game.add.sprite(1800, 400, 'react');
+
+    game.add.sprite(2000, 400, 'sass');
 
     //PLAYER DYNAMICS
 
@@ -47,6 +84,11 @@ function create() {
     //  Our two animations, walking left and right.
     player.animations.add('left', [0,1], 10, true);
     player.animations.add('right', [2,3], 10, true);
+
+    //camera follow
+    game.camera.follow(player, Phaser.Camera.FOLLOW_LOCKON, 0.1, 0.1);
+
+
 
 }
 
@@ -71,7 +113,7 @@ function update() {
     else if (cursors.right.isDown)
     {
         //  Move to the right
-        player.body.velocity.x = 150;
+        player.body.velocity.x = 350;
 
         player.animations.play('right');
     }
