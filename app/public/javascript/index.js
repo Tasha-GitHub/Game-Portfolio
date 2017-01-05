9e30f5c884a36a65aadb21fca81033c2435f7ec9
Tasha Casagni <casagni708@hotmail.com>
2017-01-05 08:51:42 -0600

    redesigning game core

--- app/public/javascript/index.js
+++ app/public/javascript/index.js
@@ -1,11 +1,21 @@
 
 var game = new Phaser.Game(1000, 600, Phaser.AUTO, "gameCore", { preload: preload, create: create, update: update });
 var player;
+var x = 3500;
+var y = 0;
+var skillCount = 0;
+var cssText;
+var projects;
+var skills;
+var projectOneLocationX = 1400;
+var projectOneLocationY = 200;
+var distanceBetweenProjects = 1000;
 
 function preload() {
 	//preloads all game elements
 	game.load.image('sky', 'images/game-core/sky.png');
     game.load.image('ground', 'images/game-core/platform.png');
+    //skill tree 
     game.load.image('js', 'images/skills-tree/javascript.png');
     game.load.image('html', 'images/skills-tree/htmlLogo.png');
     game.load.image('css', 'images/skills-tree/css.png');
@@ -17,6 +27,16 @@ function preload() {
     game.load.image('chai', 'images/skills-tree/chai.png');
     game.load.image('react', 'images/skills-tree/react.png');
     game.load.image('sass', 'images/skills-tree/sass.png');
+    //projects
+    game.load.image('bobs', 'images/game-core/projects/bobs.png');
+    game.load.image('friends', 'images/game-core/projects/friend-finder.png');
+    game.load.image('ranger', 'images/game-core/projects/hike-finder.png');
+    game.load.image('giphy', 'images/game-core/projects/widget-info.png');
+    game.load.image('trivia', 'images/game-core/projects/trivia.png');
+    game.load.image('fetch', 'images/game-core/projects/bobs.png');
+    game.load.image('rpg', 'images/game-core/projects/role-player.png');
+    game.load.image('hang-man', 'images/game-core/projects/hang-man.png');
+    //players
     game.load.spritesheet('dude', 'images/game-core/baddie.png', 32, 30);
 }
 
@@ -28,7 +48,7 @@ function create() {
     game.add.sprite(0, 0, 'sky');
 
     //world bounds
-    game.world.setBounds(0, 0, 4000, 600);
+    game.world.setBounds(0, 0, 10000, 1000);
 
     //  The platforms group contains the ground and ledges
     platforms = game.add.group();
@@ -37,37 +57,88 @@ function create() {
     platforms.enableBody = true;
 
     // Here we create the ground.
-    var ground = platforms.create(0, game.world.height - 64, 'ground');
+    var ground = platforms.create(0, game.world.height - 60, 'ground');
 
     //  Scale it to fit the width of the game
-    ground.scale.setTo(11, 2);
+    ground.scale.setTo(100, 2);
 
     //  This stops it from falling away when you jump on it
     ground.body.immovable = true;
 
-    //skill-set loaders
-    game.add.sprite(0, 400, 'js');
+    //  Now let's create ledges
+    var ledge = platforms.create(1500, 800, 'ground');
 
-    game.add.sprite(200, 400, 'html');
+    ledge.body.immovable = true;
 
-    game.add.sprite(400, 400, 'css');
+    ledge = platforms.create(1800, 670, 'ground');
 
-    game.add.sprite(600, 400, 'github');
+    ledge.body.immovable = true;
+    
+    ledge = platforms.create(1500, 500, 'ground');
 
-    game.add.sprite(800, 400, 'jquery');
+    ledge.body.immovable = true;
 
-    game.add.sprite(1000, 400, 'node');
+    ledge = platforms.create(1500, 500, 'ground');
 
-    game.add.sprite(1200, 400, 'bootstrap');
+    ledge.body.immovable = true;
 
-    game.add.sprite(1400, 400, 'mocha');
+    //projects group
+    projects = game.add.group();
 
-    game.add.sprite(1600, 400, 'chai');
+    projects.enableBody = true;
+    
+    //project Loader
+    projects.create(projectOneLocationX, projectOneLocationY, 'bobs');
 
-    game.add.sprite(1800, 400, 'react');
+    projects.create(projectOneLocationX+= distanceBetweenProjects, projectOneLocationY, 'friends');
 
-    game.add.sprite(2000, 400, 'sass');
+    projects.create(projectOneLocationX+= distanceBetweenProjects, projectOneLocationY, 'ranger');
 
+    projects.create(projectOneLocationX+= distanceBetweenProjects, projectOneLocationY, 'fetch');
+
+    projects.create(projectOneLocationX+= distanceBetweenProjects, projectOneLocationY, 'hang-man');
+
+    projects.create(projectOneLocationX+= distanceBetweenProjects, projectOneLocationY, 'giphy');
+
+    projects.create(projectOneLocationX+= distanceBetweenProjects, projectOneLocationY, 'trivia');
+
+    projects.create(projectOneLocationX+= distanceBetweenProjects, projectOneLocationY, 'rpg');
+
+
+    //skill-set group
+    skills = game.add.group();
+
+    skills.enableBody = true;
+
+    // //skill-set loaders
+    // skills.create(600, 300, 'js');
+
+    // skills.create(400, 400, 'html');
+
+    // skills.create(600, 200, 'css');
+
+    // skills.create(600, 400, 'github');
+
+    // skills.create(800, 400, 'jquery');
+
+    // skills.create(1000, 400, 'node');
+
+    // skills.create(1200, 400, 'bootstrap');
+
+    // skills.create(1400, 400, 'mocha');
+
+    // skills.create(1600, 400, 'chai');
+
+    // skills.create(1800, 400, 'react');
+
+    // skills.create(2000, 400, 'sass');
+
+    //text loaders
+    AboutMeLineOne = game.add.text(10,650, 'Hello and welcome to my portfolio game', { fontSize: '12px', fill: '#000' });
+    AboutMeLineTwo = game.add.text(10,700, 'Move the Left or Right arrows to traverse this world', { fontSize: '12px', fill: '#000' });
+    AboutMeLineThree = game.add.text(10,750, 'If you want to learn more about a project simply bump it with your head!', { fontSize: '12px', fill: '#000' });
+
+    scoreText = game.add.text(1100, 450, 'Bobs Burgers', { fontSize: '32px', fill: '#000' });
     //PLAYER DYNAMICS
 
     // The player and its settings
@@ -87,7 +158,6 @@ function create() {
 
     //camera follow
     game.camera.follow(player, Phaser.Camera.FOLLOW_LOCKON, 0.1, 0.1);
-
 
 
 }
@@ -113,7 +183,7 @@ function update() {
     else if (cursors.right.isDown)
     {
         //  Move to the right
-        player.body.velocity.x = 350;
+        player.body.velocity.x = 150;
 
         player.animations.play('right');
     }
@@ -122,7 +192,7 @@ function update() {
         //  Stand still
         player.animations.stop();
 
-        player.frame = 4;
+        player.frame = 3;
     }
 
     //  Allow the player to jump if they are touching the ground.
@@ -130,4 +200,80 @@ function update() {
     {
         player.body.velocity.y = -400;
     }
+
+    //starts collision
+    game.physics.arcade.collide(skills, platforms);
+
+    game.physics.arcade.overlap(player, skills, gainSkill, null, this);
+}
+
+
+function gainSkill (player, skill) {
+
+    // Removes the star from the screen
+    skill.kill();
+
+    // switch(skillCount) {
+    // case 0:
+    //     skills.create(x, y, "html");
+    //     x+= 10;
+    //     y+= 10;
+    //     break;
+    // case 1:
+    //     skills.create(x, y, "css");
+    //     x+= 10;
+    //     //y+= 200;
+    //     break;
+    // case 2:
+    //     skills.create(x, y, "js");
+    //     x+= 10;
+    //     //y+= 200;
+    //     break; 
+    // case 3:
+    //     skills.create(x, y, "jquery");
+    //     x+= 10;
+    //     //y+= 200;
+    //     break;
+    // case 4:
+    //     skills.create(x, y, "bootstrap");
+    //     x+= 10;
+    //     //y+= 200;
+    //     break;  
+    // case 5:
+    //     skills.create(x, y, "github");
+    //     x+= 100;
+    //     //y+= 200;
+    //     break;  
+    // case 6:
+    //     skills.create(x, y, "mocha");
+    //     x+= 10;
+    //     //y+= 200;
+    //     break;
+    // case 7:
+    //     skills.create(x, y, "chai");
+    //     x+= 10;
+    //     //y+= 200;
+    //     break;
+    // case 8:
+    //     skills.create(x, y, "react");
+    //     x+= 10;
+    //     //y+= 200;
+    //     break; 
+    // case 9:
+    //     skills.create(x, y, "sass");
+    //     x+= 10;
+    //     //y+= 200;
+    //     break;         
+
+    // default:
+    //     skills.create(x, y, "node");
+    //     x+= 10;
+    //     //y+= 100;
+       
+//}
+    skillCount++;
+    console.log(this);
+
+
+
 }
